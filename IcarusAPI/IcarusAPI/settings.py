aeb1ec1dbcbc788dab45d2f2a5bcdce27813f408
evvdd <velfimov@doublescoring.com>
2018-10-13 14:49:24 +0300

    Install rest framework and run example

--- IcarusAPI/IcarusAPI/settings.py
+++ IcarusAPI/IcarusAPI/settings.py
@@ -38,7 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'IcarusDP.apps.IcarusdpConfig',
+    'rest_framework'
 ]
+
+
+# Rest framework settings
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
