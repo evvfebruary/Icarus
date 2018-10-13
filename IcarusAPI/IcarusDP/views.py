2a55f634a2854a1398cbf7a1a7480ac306a4afa6
evvdd <velfimov@doublescoring.com>
2018-10-13 18:34:20 +0300

    Use serializers and some print debug

--- IcarusAPI/IcarusDP/views.py
+++ IcarusAPI/IcarusDP/views.py
@@ -1,11 +1,16 @@
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from IcarusDP.models import CouponSerializer
 
 
 class CampaignView(APIView):
     parser_classes = (JSONParser,)
 
     def post(self, request):
+        cpn = CouponSerializer(data=request.data)
+        print(cpn.is_valid())
+        print(cpn.errors)
+        print(cpn.validated_data)
         return Response({'received data': request.data})
 
