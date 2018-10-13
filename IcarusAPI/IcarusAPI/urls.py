15aed17e0fe1c86cf7680a57364209b34d4664b0
evvdd <velfimov@doublescoring.com>
2018-10-13 15:29:17 +0300

    Remove useless code

--- IcarusAPI/IcarusAPI/urls.py
+++ IcarusAPI/IcarusAPI/urls.py
@@ -1,22 +1,10 @@
 from django.conf.urls import url, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+
+from rest_framework import routers
 from IcarusDP.views import CampaignView
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
