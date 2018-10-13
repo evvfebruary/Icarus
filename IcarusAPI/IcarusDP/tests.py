ed12c7af8f3c8cd4a4bf163d9b263c19655d4795
evvdd <velfimov@doublescoring.com>
2018-10-13 17:06:29 +0300

    Add iata validator and create test for all function in validator module

--- IcarusAPI/IcarusDP/tests.py
+++ IcarusAPI/IcarusDP/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+import unittest
+import IcarusDP.validator as vd
+from calendar import IllegalMonthError
 
-# Create your tests here.
+class ValidatorTestCase(unittest.TestCase):
+
+    valid_iata = ['DME', "ABA", "VRN"]
+    invalid_iata = ['QQQ', 'qwedasd', 'DmE']
+    valid_datetime = map(vd.dt_tuple_format,
+                         [vd.dt_now,
+                          vd.current_datetime(vd.dt_now.year + 1, vd.dt_now.month, vd.dt_now.day)])
+
+    def test_iata_validation(self):
+        for iata in self.valid_iata:
+            self.assertTrue(vd.is_iatacode_valid(iata))
+        for iata in self.invalid_iata:
+            self.assertFalse(vd.is_iatacode_valid(iata))
+
+    def test_datetime_validation(self):
+        for datetime in self.valid_datetime:
+            self.assertTrue(vd.is_datetime_valid(datetime))
+
+        false_item = vd.dt_tuple_format(
+                           vd.current_datetime(vd.dt_now.year + 4, vd.dt_now.month, vd.dt_now.day)
+        )
+
+        self.assertFalse(vd.is_datetime_valid(false_item))
+
+        # Use calendar exception
+
+        assert_item = vd.dt_tuple_format(
+            vd.current_datetime(vd.dt_now.year, vd.dt_now.month + 10, vd.dt_now.day)
+        )
+        self.assertRaises(IllegalMonthError, vd.is_datetime_valid, assert_item)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
--- IcarusAPI/IcarusDP/validator.py
+++ IcarusAPI/IcarusDP/validator.py
@@ -2,22 +2,42 @@ import re
 from collections import namedtuple
 from datetime import datetime as dt
 from calendar import monthrange
+import pandas as pd
+import numpy as np
 MONTH_COUNT = 12
 current_datetime = namedtuple("Datetime", ['year', "month", "day"])
 dt_now = dt.now()
+
+
+def dt_tuple_format(dt_info):
+    return "{}-{}-{}".format(dt_info.year, dt_info.month, dt_info.day)
 
 
 def is_datetime_valid(datetime):
     dt_cmp = current_datetime(dt_now.year, dt_now.month, dt_now.day)
     pattern = r'[\d+]{4}-[\d+]{2}-[\d+]{2}'
     if re.match(pattern, datetime):
-        dt_request = current_datetime(*map(int,datetime.split("-")))
+        dt_request = current_datetime(*map(int, datetime.split("-")))
+        # Aviasales allows but allows to buy tickets for the year ahead
         valid_years = range(dt_cmp.year, dt_cmp.year + 2)
         valid_months = range(dt_request.month, MONTH_COUNT + 1)
+        # Monthrange return day count in month
         valid_days = range(dt_request.day, monthrange(dt_request.year, dt_request.month)[1] + 1)
         for rqst, cmp in zip(list(dt_request), [valid_years, valid_months, valid_days]):
             if rqst not in cmp:
                 return False
         return True
+    else:
+        return False
+
+
+def is_iatacode_valid(iata_code):
+    pattern = r'[A-Z]{3}'
+    if re.match(pattern, iata_code):
+        codes = np.array(pd.read_csv("../../iata_parser/iata_tables.csv")["Code"])
+        if iata_code in codes:
+            return True
+        else:
+            return False
     else:
         return False
